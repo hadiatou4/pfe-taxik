65817747a5d6f4b16e6f1cba723516ff83151578
Hadiatou Keita <haadikeita4@gmail.com>
2024-05-03 16:01:33 +0100

    creation du formulaire d'inscription

---
+++ app/components/RegistrationForm.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function RegistrationForm() {
+  return (
+    <div className='bg-white m-auto'>
+      <h1>Créer votre compte </h1>
+      <form method='POST'>
+        
+      </form>
+    </div>
+  )
+}
+
+export default RegistrationForm
